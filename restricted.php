d83b03d4718656c25d2c84a13dada6adc852e137
gopherproxy <gopherproxy@gmail.com>
2015-04-29 11:33:53 +0200

    logout

--- restricted.php
+++ restricted.php
@@ -1,3 +1,9 @@
+<?php
+// taking up the session
+session_start();
+// checking session ID
+if ($_SESSION['logged_in'] == true){ 
+?>
 <!doctype html>
 <html>
 <head>
@@ -7,5 +13,13 @@
 
 <body>
 <h1>Only for very special people (aliens, bulgarians and the like)</h1>
+<a href="logout.php">Logout</a>
 </body>
 </html>
+<?php
+// closing if condition from above
+}
+else {
+    header("location:login.php");
+}
+?>
